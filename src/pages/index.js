e2d5296655ab9b7c32664a37cd0ec0923d22b467
brittani1128 <brittani1128@gmail.com>
2020-07-29 23:33:36 -0500

    add hero component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,17 +3,13 @@ import React from "react"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Hero from "../components/hero"
 
-const Home = () => (
+const Index = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, margin: "auto" }}>
-      <Image />
-    </div>
+    <Hero />
   </Layout>
 )
 
-export default Home
+export default Index
