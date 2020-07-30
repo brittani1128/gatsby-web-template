5f9080dc504b2e71390b6083fdb7f74c5d4a560d
brittani1128 <brittani1128@gmail.com>
2020-07-29 20:48:25 -0500

    add navigation component

---
+++ src/components/navigation.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react"
+import { Link } from "gatsby"
+
+import style from "../styles/layout.module.css"
+
+export default class Navigation extends Component {
+  render() {
+    return (
+      <nav className={style.nav}>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/blog">Blog</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+    )
+  }
+}
